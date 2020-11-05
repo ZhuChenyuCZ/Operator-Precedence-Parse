7f596fd4c97c6e82ec1a6eec0058a879ac86f64a
ZhuChenyuCZ <1125664127@qq.com>
2020-11-05 20:13:43 +0800

    本地测试完成大部分功能

--- OPG.c
+++ OPG.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
-#include<string.h>
-FILE *in;
-char c;
-int main(int argc, char* argv[]){
-    //in=fopen(argv[1],"r");
-    in=fopen("test.in","r");
-    c=fgetc(in);
-    while(c!=EOF){
-        printf("%c",c);
-        c=fgetc(in);
-    }
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char line[2550];
+
+int main(int argc,char* argv[])
+{
+    FILE *fp = NULL;
+    fp = fopen("test.in", "r");
+    fgets(line,2000,(FILE *)fp);
+    printf(line);
+    fclose(fp);
 }
